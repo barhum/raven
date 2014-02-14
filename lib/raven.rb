83af966ed0924e0aa8819cf21a9941a4413c59d2
Moneer <mbcomm@gmail.com>
2014-02-13 23:48:30 -0600

    indent hell

--- lib/raven.rb
+++ lib/raven.rb
@@ -4,25 +4,25 @@ require 'debugger'
 module Raven
 	class Raven
 		def initialize(operation)
-      @values = {}
-      if !self.ravenOperations.include?(operation.to_s)
-        raise "Operation not supported"
-      end         
-	  end
+			@values = {}
+			if !self.ravenOperations.include?(operation.to_s)
+				raise "Operation not supported"
+			end         
+		end
 
-	  def ravenOperations
-	  	@ravenOperations = ['submit','closefile','response','void','hello','payments','events','status']
-	  end	 
+		def ravenOperations
+			@ravenOperations = ['submit','closefile','response','void','hello','payments','events','status']
+		end	 
 
-	  def values
-      @values
-	  end	
+		def values
+			@values
+		end	
 	end
 
 	class RavenRequest < Raven
 		def set(key, value)
-      @values[key] = value.to_s
-    end
+			@values[key] = value.to_s
+		end
 	end	
 end	
 
