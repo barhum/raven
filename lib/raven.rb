a6a1a59df46f515d3cbeea6ec72bbc88a727a72a
Moneer <mbcomm@gmail.com>
2014-02-16 09:40:38 -0600

    exception classes

--- lib/raven.rb
+++ lib/raven.rb
@@ -2,13 +2,65 @@ require 'config'
 require 'debugger'
 
 module Raven
+  class RavenException < Exception
+    "A problem has occurred with some aspect of Raven processing."
+    #
+    # Raven is not properly configured, probably due to a missing configuration 
+    # file or absent mandatory configuration parameter (see 
+    # http://docs.deepcovelabs.com/raven/api-guide/).
+    #  
+  end
+  
+  class RavenConfigurationException < RavenException
+    " Raven is not properly configured, probably due to a missing
+    configuration file or absent mandatory configuration parameter 
+    (see http://docs.deepcovelabs.com/raven/api-guide/)."
+
+    #
+    # A requested Raven operation is not defined in the targeted version of
+    # the API (as specified by the request parameter "RAPIVersion").
+    # (see http://docs.deepcovelabs.com/raven/api-guide/).
+    #
+  end
+
+  class RavenNoSuchOperationException < RavenException
+        " A requested Raven operation is not defined in the targeted version 
+    of the API, as specified by the request parameter #{'insert rapiversion here'} (see
+    http://docs.deepcovelabs.com/raven/api-guide/)."
+
+    # This exception will be thrown if no response was received from the Raven
+    # system. This may be due to network issues or if there was a server 500 
+    # errror.
+    # 
+    # If this exception is thrown you should make a response request with the 
+    # RequestID used (see http://docs.deepcovelabs.com/raven/api-guide/).
+    #
+  end
+
+  class RavenNoResponseException < RavenException
+    " This exception will be thrown if no response was received from the
+    Raven system. This may be due to network issues or if there was a 
+    server 500 errror. If this exception is thrown you should make a
+    response request with the RequestID used (see
+    http://docs.deepcovelabs.com/raven/api-guide/)."
+
+    # A Raven operation or response was not properly authenticated.
+  end
+
+  class RavenAuthenticationException < RavenException
+    " A Raven operation or response was not properly authenticated."
+
+    # Abstract class to handle common functionality between RavenRequest and 
+    # RavenResponse.     
+  end  
+
   class Raven
     attr_reader :values, :operation
 
     def initialize(operation)
       @values = {}
       if !self.ravenOperations.include?(operation.to_s)
-        raise "Operation not supported"
+        raise RavenNoSuchOperationException("#{operation} is an unsupported operation.")
       end
       @operation = operation         
     end
