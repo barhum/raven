a125c350727f1d8657ba172614ba0cffe660615b
Moneer <mbcomm@gmail.com>
2014-03-25 12:28:41 -0600

    more changes

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,8 +1,6 @@
 class InitializerGenerator < Rails::Generators::Base
   def create_initializer_file
     create_file "config/initializers/initializer.rb" do 
-      att_reader :ravenUser
-
       def ravenUser
         @ravenUser = 'ernest'
       end  
