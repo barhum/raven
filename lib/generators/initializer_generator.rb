a4167ab66ee27e4234d255da8e4a420e35e3f8a3
Moneer <mbcomm@gmail.com>
2014-03-25 12:38:01 -0600

    more changes, like 2pac

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,9 +1,7 @@
 class InitializerGenerator < Rails::Generators::Base
   def create_initializer_file
     create_file "config/initializers/initializer.rb" do 
-      def ravenUser
-        @ravenUser = 'ernest'
-      end  
+      @ravenUser = 'ernest' 
     end  
   end
 end
