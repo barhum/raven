b9e0c3862ffc86617b081cb0d982321e6ca99448
Moneer <mbcomm@gmail.com>
2014-03-25 12:52:45 -0600

    more options

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,8 +1,13 @@
 class InitializerGenerator < Rails::Generators::Base
   def create_initializer_file
     create_file "config/initializers/raven_config.yml" do 
-      "user: ernest
-       secret: all good men die young"
+      "user: ernest \n
+       secret: all good men die young \n
+       gateway: https://demo.pacnetservices.com/realtime \n
+       prefix: TEST \n
+       raven_debug: off \n
+       rapi_version: 2 \n
+       rapi_interface: rails0.1"
     end  
   end
 end
