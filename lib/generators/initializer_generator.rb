e3e9b3fd040a22d6885ded3089204227c15ebf7c
Moneer <mbcomm@gmail.com>
2014-03-25 12:48:56 -0600

    more changes

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,8 +1,8 @@
 class InitializerGenerator < Rails::Generators::Base
   def create_initializer_file
     create_file "config/initializers/raven_config.yml" do 
-      "user: ernest"
-      "secret: all good men die young"
+      "user: ernest
+       secret: all good men die young"
     end  
   end
 end
