dfa92f93d0c72e0729f5e7ca42db60dc9f5fb3b1
Moneer <mbcomm@gmail.com>
2014-03-25 12:12:58 -0600

    generator settings

--- lib/raven.rb
+++ lib/raven.rb
@@ -7,7 +7,7 @@ module Raven
   
   class InitializerGenerator < Rails::Generators::Base
     def create_initializer_file
-      create_file "config/initializers/raven_init.yml", "# Add initialization content here"
+      create_file "config/initializers/initializer.rb", "# Add initialization content here"
     end
   end
 
