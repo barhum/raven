93ce34691709e899e75f65690790685acfbc91e6
Moneer <mbcomm@gmail.com>
2014-03-25 13:59:23 -0600

    remove yaml

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -9,7 +9,6 @@ class InitializerGenerator < Rails::Generators::Base
       @raven_debug = 'off' 
       @rapi_version =  2 
       @rapi_interface = 'rails0.1'")
-    end  
     out_file.close
   end
 end
