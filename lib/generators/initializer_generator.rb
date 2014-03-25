e7a9a1023f1fb2ab2b756872fb393f57de4fe9ac
Moneer <mbcomm@gmail.com>
2014-03-25 13:53:28 -0600

    remove yaml

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,14 +1,14 @@
 class InitializerGenerator < Rails::Generators::Base
   def create_initializer_file
     out_file = File.new("config/initializers/raven_config.rb", "w")
-    out_file.write do 
-      @user = 'ernest'
+    out_file.puts do 
+     "@user = 'ernest'
       @secret = 'all good men die young'
       @gateway =  'https://demo.pacnetservices.com/realtime'
       @prefix = 'TEST'
       @raven_debug = 'off' 
       @rapi_version =  2 
-      @rapi_interface = "rails0.1"
+      @rapi_interface = 'rails0.1'"
     end  
     out_file.close
   end
