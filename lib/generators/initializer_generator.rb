a288df8a1464b09cd0b338fe189846339259275c
Moneer <mbcomm@gmail.com>
2014-03-25 14:40:17 -0600

    fix attr_reader

--- lib/generators/initializer_generator.rb
+++
@@ -1,9 +0,0 @@
-class InitializerGenerator < Rails::Generators::Base
-  RavenConfig.setUser('ernest')
-  RavenConfig.setSecret('all good men die young')
-  RavenConfig.setGateway("https://demo.pacnetservices.com/realtime")
-  RavenConfig.setPrefix("TEST")
-  RavenConfig.setDebug('off')
-end
-
-
